5a14973114752ea1e68cb99a21cc4456a58f8720
cruii <cruii811@gmail.com>
2023-08-28 02:09:21 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import AuthenticationPage from "@/pages/AuthForm.tsx";
+// import Home from "@/Home.tsx";
+
+function App() {
+
+  return (
+    <>
+      {/*<Home />*/}
+      <AuthenticationPage />
+    </>
+  )
+}
+
+export default App
