2b82844f2eedf6dfd2a725b464b5770ede181fa0
Cruii <cruii811@gmail.com>
2023-08-30 01:27:41 +0800

    update ui

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,12 +1,12 @@
 export const siteConfig = {
-  name: "Oh My Helper",
-  url: "https://ohmyhelper.com",
-  ogImage: "https://ui.shadcn.com/og.jpg",
+  name: 'Oh My Helper',
+  url: 'https://ohmyhelper.com',
+  ogImage: 'https://ui.shadcn.com/og.jpg',
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    'Beautifully designed components built with Radix UI and Tailwind CSS.',
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn-ui/ui",
+    twitter: 'https://twitter.com/shadcn',
+    github: 'https://github.com/shadcn-ui/ui',
   },
 }
 
--- src/mocks/auth.ts
+++ src/mocks/auth.ts
@@ -1,13 +1,13 @@
-import Mock, {Random} from 'mockjs'
+import Mock, { Random } from 'mockjs'
 
 export const preAuth = Mock.mock('/api/user/preauth', 'get', {
   hasAccount: Random.boolean(),
 })
 
 export const login = Mock.mock('/api/user/login', 'post', {
-  token: 'ehgkaaDGdkaqeAWRnaskdASDffdfdfavmkop'
+  token: 'ehgkaaDGdkaqeAWRnaskdASDffdfdfavmkop',
 })
 
 export const signup = Mock.mock('/api/user/signup', 'post', {
-  token: 'ehgkaaDGdkaqeAWRnaskdASDffdfdfavmkopsignup'
+  token: 'ehgkaaDGdkaqeAWRnaskdASDffdfdfavmkopsignup',
 })
